1e41c56a81471e5bb02261563435a7c53bf1df8f
Him Kishan Das <himkishandas456@gmail.com>
2024-08-06 19:27:45 +0530

    development phase

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,8 +1,8 @@
-// window.addEventListener('load', () => {
-//     const intro = document.getElementById('welcomeScreen');
-//     const content = document.getElementById('container');
-//     setTimeout(() => {
-//         intro.classList.add('hidden');
-//         content.classList.add('visible');
-//     }, 1500);
-// });
+window.addEventListener('load', () => {
+    const intro = document.getElementById('welcomeScreen');
+    const content = document.getElementById('container');
+    setTimeout(() => {
+        intro.classList.add('hidden');
+        content.classList.add('visible');
+    }, 2500);
+});
