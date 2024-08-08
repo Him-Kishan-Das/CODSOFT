981e4a590f3c88f4d594ae338e3f7217178426a4
Him Kishan Das <himkishandas456@gmail.com>
2024-08-09 00:56:21 +0530

    development

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -6,3 +6,11 @@ window.addEventListener('load', () => {
         content.classList.add('visible');
     }, 2500);
 });
+
+// const burger = document.querySelector('.burger');
+//         const nav = document.querySelector('#nav-links');
+
+//         burger.addEventListener('click', () => {
+//             nav.classList.toggle('active');
+//             burger.classList.toggle('active');
+//         });
